a65fbb06d49d456dc42f352f77109923992e4ae2
Mike S <SpainTrain@users.noreply.github.com>
2017-06-12 02:04:11 -0400

    :fire: Removed chai-as-promised (fixes #10) (#11)
    - Removed chai-as-promised
    - Updates dependencies
    
    release v+major
    
    Closes #10

--- src/index.js
+++ src/index.js
@@ -9,10 +9,8 @@ try {
 import chai from 'chai'
 import 'sinon'
 import sinonChai from 'sinon-chai'
-import chaiAsPromised from 'chai-as-promised'
 
 chai.use(sinonChai)
-chai.use(chaiAsPromised)
 
 export {expect} from 'chai'
 export {default as jsc} from 'jsverify'
